9f11fcd99b87dbc6d7fdf061dabe55cf0e22a4f7
MC1905 <94351375+MC1905@users.noreply.github.com>
2023-09-20 15:09:36 +0200

    push

--- C#_p3/Program.cs
+++ C#_p3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("test push");
+﻿using System;
+using System.Collections.Generic;
+
+// Klasse voor het vertegenwoordigen van een werknemer
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public string Name { get; set; }
+    public decimal LeaveBalance { get; set; }
+}
+
