ed401c75f7d17ddc4ddabd308edd3e3bc5da660b
MC1905 <94351375+MC1905@users.noreply.github.com>
2023-09-22 14:13:54 +0200

    push

--- C#_p3/Program.cs
+++ C#_p3/Program.cs
@@ -9,3 +9,64 @@ public class Employee
     public decimal LeaveBalance { get; set; }
 }
 
+// Klasse voor het vertegenwoordigen van een verlofaanvraag
+public class LeaveRequest
+{
+    public int LeaveRequestId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Reason { get; set; }
+    public bool Approved { get; set; }
+}
+
+// Klasse voor het vertegenwoordigen van een manager
+public class Manager
+{
+    public int ManagerId { get; set; }
+    public string Name { get; set; }
+
+    // Methode om verlofaanvragen goed te keuren
+    public void ApproveLeaveRequest(LeaveRequest request)
+    {
+        // Voer goedkeuringslogica uit
+        request.Approved = true;
+    }
+}
+
+// Klasse voor het verlofregistratiesysteem
+public class LeaveManagementSystem
+{
+    private List<Employee> employees = new List<Employee>();
+    private List<LeaveRequest> leaveRequests = new List<LeaveRequest>();
+    private List<Manager> managers = new List<Manager>();
+
+    // Methode om een nieuwe verlofaanvraag in te dienen
+    public void SubmitLeaveRequest(int employeeId, DateTime startDate, DateTime endDate, string reason)
+    {
+        // Voer validatie en opslag van de aanvraag uit
+        var request = new LeaveRequest
+        {
+            LeaveRequestId = leaveRequests.Count + 1,
+            EmployeeId = employeeId,
+            StartDate = startDate,
+            EndDate = endDate,
+            Reason = reason,
+            Approved = false
+        };
+
+        leaveRequests.Add(request);
+    }
+
+    // Methode om een manager toe te voegen
+    public void AddManager(int managerId, string name)
+    {
+        managers.Add(new Manager { ManagerId = managerId, Name = name });
+    }
+
+    // Methode om een werknemer toe te voegen
+    public void AddEmployee(int employeeId, string name, decimal leaveBalance)
+    {
+        employees.Add(new Employee { EmployeeId = employeeId, Name = name, LeaveBalance = leaveBalance });
+    }
+}
